f56ecf416c7a959564a513a530852259dfc23769
Rob Holden <robholdenflag@gmail.com>
2017-01-16 16:47:20 +0000

    Installed sinatra, thin and shotgun. Created different route's including /cat which loads a .png of a cat

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "hello!"
+end
+
+get '/secret'do
+  "There is always money in the banana stand"
+end
+
+get '/teeth' do
+  "Sugar is cancer"
+end
+
+get '/sleep' do
+  "Go to bed"
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
