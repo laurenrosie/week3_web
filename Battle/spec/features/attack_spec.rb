129b54bc2fed152b779bc64c6728c01c485fc2bb
Rob Holden <robholdenflag@gmail.com>
2017-01-18 15:54:37 +0000

    Added an attack button

---
+++ Battle/spec/features/attack_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+
+feature "Players attacking" do
+
+  scenario "Expect players to attack and confirm" do
+    sign_in_and_play
+    click_button 'Attack player 2'
+    expect(page).to have_content 'Attacked player 2!'
+  end
+end
--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -4,10 +4,7 @@ require 'spec_helper'
 feature "Players filling name" do
 
   scenario "Expect players to fill form" do
-    visit('/')
-      fill_in :player_1_name, with: 'Rob'
-      fill_in :player_2_name, with: 'Lauren'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Rob vs. Lauren'
   end
 end
