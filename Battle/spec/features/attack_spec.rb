b3026c0e5d7395527092650d56d6727f346638f8
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-19 11:50:53 +0000

    files from yesterday

--- Battle/spec/features/attack_spec.rb
+++ Battle/spec/features/attack_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 feature "Players attacking" do
-  binding.pry
   scenario "Expect players to attack and confirm" do
     sign_in_and_play
     click_button 'Attack player 2'
