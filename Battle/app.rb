2759a7330abd8faff8a2855b28c124fdce725eb2
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-01-19 15:18:54 +0000

    Put players into game

--- Battle/app.rb
+++ Battle/app.rb
@@ -10,27 +10,23 @@ class Battle < Sinatra::Base
 
   post '/names' do
     if !params[:attacker]
-      $player_1 = Player.new(params[:player_1_name])
-      $player_2 = Player.new(params[:player_2_name])
+      $game = Game.new(params[:player_1_name], params[:player_2_name])
     end
     $attacker = params[:attacker]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1_name = $game.player1.name
+    @player_2_name = $game.player2.name
     if $attacker
-      Game.new.attack($player_2)
+      $game.attack($game.player2)
     end
-    @p1_points = $player_1.points
-    @p2_points = $player_2.points
+    @p1_points = $game.player1.points
+    @p2_points = $game.player2.points
     @attacker = $attacker
     erb :play
   end
-
-
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- Battle/spec/game_spec.rb
+++ Battle/spec/game_spec.rb
@@ -1,10 +1,15 @@
 require 'game'
 
 describe Game do
+
+  let(:player1) {double Player}
+  let(:player2) {double Player}
+  subject(:game) {described_class.new(player1, player2)}
+
   describe '#attack' do
     it "takes one parameter" do
       expect(subject).to respond_to(:attack).with(1).argument
     end
-    
+
   end
 end
